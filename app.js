a78fcb50c49f9567dd3b1ad1edd058a0c79da4ad
Zero <Zeroto521@gmail.com>
2021-03-08 11:14:17 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const process = require('process')
+const express = require('express');
+const process = require('process');
 
-const app = express()
+const app = express();
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
+
+app.get('/health', (_, res) => {
+  res.send('ok');
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
-})
+  console.log('server started on port 5000');
+});
